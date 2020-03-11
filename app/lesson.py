d65bd9fb9d8340acdeb8694c8c2e0921625b1dff
Vasiliy Samoylov <vasiliy.samoylov@x-tensive.com>
2020-03-11 17:52:36 +0700

    show room and group

--- app/lesson.py
+++ app/lesson.py
@@ -1,19 +1,23 @@
 from app.days import *
 from app.subtime import *
 from app.subject import *
+from app.room import *
+from app.group import *
 
 
 class Lesson(object):
-    def __init__(self, weekday, subtime, subject):
+    def __init__(self, weekday, subtime, subject, room, group):
         self.weekday = weekday
         self.subtime = subtime
         self.subject = subject
+        self.room = room
+        self.group = group
 
 
 lessons = [
-    Lesson(monday, first_subtime, olymp_prog),
-    Lesson(monday, second_subtime, programming),
-    Lesson(tuesday, first_subtime, programming),
-    Lesson(wednesday, second_subtime, programming),
-    Lesson(thursday, first_subtime, programming)
+    Lesson(monday, first_subtime, olymp_prog, room453, group10),
+    Lesson(monday, second_subtime, programming, room453, group10),
+    Lesson(tuesday, first_subtime, programming, room454a, group8),
+    Lesson(wednesday, second_subtime, programming, room453, group10),
+    Lesson(thursday, first_subtime, programming, room453, group8)
 ]
